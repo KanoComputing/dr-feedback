eca012cb9cb206a5381b6f8248872f857e6734a8
Tom Bettany <tom.bettany@gmail.com>
2019-07-08 11:42:29 +0100

    Lint: Fix linting errors
    Fix problems which prevent proper linting.

--- monitor/drfeedback-monitor.py
+++ monitor/drfeedback-monitor.py
@@ -1,23 +1,28 @@
+#!/usr/bin/env python
 #
-#  drfeedback-monitor.py
+# drfeedback-monitor.py
 #
-#  Copyright (C) 2019 Kano Computing Ltd.
-#  License:   http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+# Copyright (C) 2019 Kano Computing Ltd.
+# License:   http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
 #
-#  This module keeps track of DrFeedback alerts received from end users,
-#  and raises an alert to the Kano Team in case a spike is detected,
-#  suggesting that a major problem might have raised, and detailed look should be addressed.
+# This module keeps track of DrFeedback alerts received from end users,
+# and raises an alert to the Kano Team in case a spike is detected,
+# suggesting that a major problem might have raised, and detailed look should
+# be addressed.
 #
-#  The functionality is divided in two steps:
+# The functionality is divided in two steps:
 #
-#   1) Collect and save a historic log with number of alerts received to the server
-#   2) Read and process this log to detect any spike overtime, send and alert in such cases
+#  1) Collect and save a historic log with number of alerts received to the
+#     server
+#  2) Read and process this log to detect any spike overtime, send and alert in
+#     such cases
 #
-#  The "process" command will return a1 if a spike is detected.
+# The "process" command will return a1 if a spike is detected.
 #
 
 """
-DRFeedback-monitor keeps track and alerts the Kano Team about potential Kano OS customer troubles
+DRFeedback-monitor keeps track and alerts the Kano Team about potential Kano OS
+customer troubles
 
 Usage:
   drfeedback-monitor collect --logfile=<filename> --directory=<pathname> [--commit]
@@ -29,41 +34,49 @@ Options:
    -a, --alert                  Send a slack alert if spike is detected
    -V, --verbose                Explain the steps taken during the process
    -l, --logfile=<filename>     Name of logfile to collect data into or process
-   -d, --directory=<pathname>   Directory to check for numbers of files and collect counter
-   -s, --samples=<n>            Number of samples to group for computing average
-   -t, --threshold=<factor>     Threshold multitplier for the last average, if above then spike is raised
+   -d, --directory=<pathname>   Directory to check for numbers of files and
+                                collect counter
+   -s, --samples=<n>            Number of samples to group for computing
+                                average
+   -t, --threshold=<factor>     Threshold multitplier for the last average, if
+                                above then spike is raised
 
 """
-
-LOGFILE = 'drfeedback-numbers.txt'
-DRFEEDBACK_DIRECTORY = '/var/cache'
-SAMPLES = 5
-THRESHOLD = 1.5
 
 import os
 import sys
 from time import gmtime, strftime
 import docopt
 
+LOGFILE = 'drfeedback-numbers.txt'
+DRFEEDBACK_DIRECTORY = '/var/cache'
+SAMPLES = 5
+THRESHOLD = 1.5
 
-class Collect():
+
+class Collect(object):
     '''
-    Collects the current number of Dr Feedback alerts and saves it into a logfile
+    Collects the current number of Dr Feedback alerts and saves it into a
+    logfile
     '''
     def __init__(self, args):
         self.args = args
-        self.collect_command = 'ls -l {} | wc -l'.format(self.args['--directory'])
+        self.collect_command = 'ls -l {} | wc -l'.format(
+            self.args['--directory']
+        )
 
     def vprint(self, args):
         if self.args['--verbose']:
             print args
-        
+
     def _collect_counter_(self):
         '''
         Obtains the number of drfeedback alerts
         '''
         log_count = os.popen(self.collect_command).read().strip()
-        return 'date={} total-logs={}'.format(strftime("%d-%m-%Y", gmtime()), log_count)
+        return 'date={} total-logs={}'.format(
+            strftime("%d-%m-%Y", gmtime()), log_count
+        )
 
     def _save_log_(self, entry):
         '''
@@ -76,17 +89,20 @@ class Collect():
         '''
         Run is the main entry point to put this class task to work
         '''
-        log_entry=self._collect_counter_()
-        self.vprint ('Collected counter: {}'.format(log_entry))
+        log_entry = self._collect_counter_()
+        self.vprint('Collected counter: {}'.format(log_entry))
 
         if self.args['--commit']:
             self._save_log_(log_entry)
-            self.vprint ('Commited counter to logfile: {}'.format(self.args['--logfile']))
+            self.vprint(
+                'Commited counter to logfile: {}'
+                .format(self.args['--logfile'])
+            )
 
         return 0
 
 
-class Process():
+class Process(object):
     '''
     Reads and processes a logfile, sends an alert if a spike is detected.
     '''
@@ -106,29 +122,29 @@ class Process():
         '''
         counters = []
         self.vprint('reading logfile {}'.format(self.args['--logfile']))
-        with open (self.args['--logfile'], 'r') as f:
-            logdata = f.readlines()
+        with open(self.args['--logfile'], 'r') as log_f:
+            logdata = log_f.readlines()
 
-        for l in logdata:
-            counters.append (int(l.split('=')[2]))
+        for log in logdata:
+            counters.append(int(log.split('=')[2]))
 
-        self.vprint ('collected {} drfeedback alerts'.format(len(counters)))
+        self.vprint('collected {} drfeedback alerts'.format(len(counters)))
         return counters
 
     def _compute_(self, numbers):
         '''
-        Reads a list of counters obtained from the logfile, and detects a spike,
-        given the number of samples to average, and a threshold to compute the maximum deviation allowed
+        Reads a list of counters obtained from the logfile, and detects a
+        spike, given the number of samples to average, and a threshold to
+        compute the maximum deviation allowed
 
         Returns True if a spike is detected, False otherwise
         '''
         samples = []
         last_average_value = 0
         last_counter = 0
-        
-        for index, counter in enumerate(reversed(numbers)):
 
-            #self.vprint ('>>> {} {}'.format(counter, index))
+        for index, counter in enumerate(reversed(numbers)):
+            # self.vprint('>>> {} {}'.format(counter, index))
             if not last_counter:
                 last_counter = counter
                 continue
@@ -139,18 +155,26 @@ class Process():
             if (index + 1) % self.args['--samples'] == 0:
 
                 average_alerts = sum(samples) / len(samples)
-                self.vprint ('Average number of drfeedback alerts is {} over last {} samples'.format(
-                    average_alerts, self.args['--samples']))
+                self.vprint(
+                    'Average number of drfeedback alerts is {} over last {} '
+                    'samples'.format(average_alerts, self.args['--samples']))
 
                 if not last_average_value:
                     last_average_value = average_alerts
                 else:
 
                     if self._detect_spike(average_alerts, last_average_value):
-                        self.vprint ('A spike was detected! current-average={} last-average={} ' \
-                                     'threshold={} max-permitted-average={}'.format(
-                                         average_alerts, last_average_value,
-                                         self.args['--threshold'], average_alerts * self.args['--threshold']))
+                        self.vprint(
+                            'A spike was detected! '
+                            'current-average={} last-average={} '
+                            'threshold={} max-permitted-average={}'.format(
+                                average_alerts,
+                                last_average_value,
+                                self.args['--threshold'],
+                                average_alerts * self.args['--threshold']
+                            )
+                        )
+
                         return True
                     else:
                         last_average_value = average_alerts
@@ -162,28 +186,45 @@ class Process():
         '''
         Returns True if spike is detected
         '''
-        self.vprint ('Spike check: current-average={} last-average={} threshold={} max-permitted-average={}'.format(
-                        average_alerts, last_average, self.args['--threshold'], average_alerts * self.args['--threshold']))
-        
+        self.vprint(
+            'Spike check: current-average={} last-average={} threshold={} '
+            'max-permitted-average={}'.format(
+                average_alerts,
+                last_average,
+                self.args['--threshold'],
+                average_alerts * self.args['--threshold']
+            )
+        )
+
         is_spike = average_alerts * self.args['--threshold'] < last_average
         if is_spike:
-            self.vprint ('Spike was detected! {} * {} < {}'.format(average_alerts, self.args['--threshold'], last_average))
+            self.vprint(
+                'Spike was detected! {} * {} < {}'
+                .format(
+                    average_alerts, self.args['--threshold'], last_average
+                )
+            )
 
         return is_spike
 
     def slack_alert(self, simulate):
         '''
-        Sends a network slack alert to the Dev Team to bring attentio to DrFeedback logs
+        Sends a network slack alert to the Dev Team to bring attention to
+        DrFeedback logs
         '''
 
         url = self.url_simulate if simulate else self.url_slack
-        self.vprint('Slack alert signal requested, simulate={} url={}'.format(simulate, url))
+        self.vprint(
+            'Slack alert signal requested, simulate={} url={}'
+            .format(simulate, url)
+        )
 
-        command = 'curl -X POST --data-urlencode \'payload=' \
-                  '{"channel": "#team-os", "username": "drfeedback", ' \
-                  '"text": "DrFeedback Monitor detected a spike: ' \
-                  'https://kanocomputing.atlassian.net/wiki/spaces/OS/pages/422150149/Dr+Feedback+Architecture", ' \
-                  '"icon_emoji": ":face_with_monocle:"}\' ' + url
+        command = \
+            'curl -X POST --data-urlencode \'payload=' \
+            '{"channel": "#team-os", "username": "drfeedback", ' \
+            '"text": "DrFeedback Monitor detected a spike: ' \
+            'https://kanocomputing.atlassian.net/wiki/spaces/OS/pages/422150149/Dr+Feedback+Architecture", ' \
+            '"icon_emoji": ":face_with_monocle:"}\' ' + url
 
         self.vprint(command)
 
@@ -200,42 +241,38 @@ class Process():
         counters = self._get_log_counters_()
         return self._compute_(counters)
 
-        
-
-
 
 if __name__ == '__main__':
+    ARGS = docopt.docopt(__doc__)
+    RC = 0
 
-    args = docopt.docopt(__doc__)
-    rc = 0
+    if ARGS['--verbose']:
+        print ARGS
 
-    if args['--verbose']:
-        print args
-    
     # Provide default values when not specified
-    if not args['--directory']:
-        args['--directory'] = DRFEEDBACK_DIRECTORY
+    if not ARGS['--directory']:
+        ARGS['--directory'] = DRFEEDBACK_DIRECTORY
 
-    if not args['--logfile']:
-        args['--logfile'] = LOGFILE
+    if not ARGS['--logfile']:
+        ARGS['--logfile'] = LOGFILE
 
-    if not args['--samples']:
-        args['--samples'] = int(SAMPLES)
+    if not ARGS['--samples']:
+        ARGS['--samples'] = int(SAMPLES)
     else:
-        args['--samples'] = int(args['--samples'])
+        ARGS['--samples'] = int(ARGS['--samples'])
 
-    if not args['--threshold']:
-        args['--threshold'] = float(THRESHOLD)
+    if not ARGS['--threshold']:
+        ARGS['--threshold'] = float(THRESHOLD)
     else:
-        args['--threshold'] = float(args['--threshold'])
+        ARGS['--threshold'] = float(ARGS['--threshold'])
 
-    if args['process']:
-        processor = Process(args)
-        rc = processor.run()
-        if rc:
-            processor.slack_alert(simulate = not args['--alert'])
+    if ARGS['process']:
+        PROCESSOR = Process(ARGS)
+        RC = PROCESSOR.run()
+        if RC:
+            PROCESSOR.slack_alert(simulate=not ARGS['--alert'])
 
-    elif args['collect']:
-        rc = Collect(args).run()
+    elif ARGS['collect']:
+        RC = Collect(ARGS).run()
 
-    sys.exit(rc)
+    sys.exit(RC)
